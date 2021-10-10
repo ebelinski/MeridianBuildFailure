ceb12efd04560316a3e6dc1761550d39f6f9e7aa
Eugene Belinski <ebelinski@protonmail.com>
2021-10-10 13:38:41 -0500

    initial commit

---
+++ MeridianBuildFailure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeridianBuildFailure
+//
+//  Created by Eugeniu Belinski on 10/10/21.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
